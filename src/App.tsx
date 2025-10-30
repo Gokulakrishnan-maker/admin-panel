d9fcf39014f0f2f1c344f345a17e126a48f3f5b6
Gokulakrishnan-maker <gokulakrishnan756@gmail.com>
2025-10-30 20:23:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Invoices from "./pages/Invoices";
+import Rides from "./pages/Rides";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/" element={<Layout />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="invoices" element={<Invoices />} />
-          <Route path="logout" element={<div>Logging out...</div>} />
+          <Route path="rides" element={<Rides />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
+
 
 export default App;
