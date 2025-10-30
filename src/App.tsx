d693886b80e64aac6c4f68d907b3a9184e599055
Gokulakrishnan-maker <gokulakrishnan756@gmail.com>
2025-10-30 20:25:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Invoices from "./pages/Invoices";
 import Rides from "./pages/Rides";
+import Layout from "./components/Layout";
+import { AuthProvider } from "./context/AuthContext";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/" element={<Layout />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="invoices" element={<Invoices />} />
-          <Route path="rides" element={<Rides />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route element={<Layout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/invoices" element={<Invoices />} />
+            <Route path="/rides" element={<Rides />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
-
 
 export default App;
