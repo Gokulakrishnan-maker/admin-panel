0346e0c6bcc9a0ce989975df7212bbe221937b08
Gokulakrishnan-maker <gokulakrishnan756@gmail.com>
2025-12-29 17:24:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Invoices from "./pages/Invoices";
 import Rides from "./pages/Rides";
 import Layout from "./components/Layout";
 import { AuthProvider } from "./context/AuthContext";
+import Rates from "./pages/Rates";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/invoices" element={<Invoices />} />
             <Route path="/rides" element={<Rides />} />
-          </Route>
+            <Route path="/rates" element={<Rates />} />
+            </Route>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
